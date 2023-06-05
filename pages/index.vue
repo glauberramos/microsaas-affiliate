bf1c8734bba63155bb8826c9efcabf57a399d03f
Glauber Ramos <glauberamos@gmail.com>
2023-06-05 00:30:13 -0300

    First version

---
+++ pages/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <div class="flex-grow flex">
+      <!-- Your existing code -->
+      <div class="p-4">
+        <h1
+          class="p-4 mt-3 text-3xl font-extrabold tracking-tight text-slate-900"
+        >
+          MicroSaaS Affiliate Program Directory
+        </h1>
+        <div class="flex flex-wrap">
+          <div v-for="app in apps" :key="app.name" class="w-1/3 p-4">
+            <div class="flex items-start p-2 bg-white rounded shadow">
+              <img :src="app.logo" class="w-12 h-12 ml-2 mr-3 p-1" alt="logo" />
+              <div>
+                <h2 class="text-lg font-semibold">
+                  <a
+                    :href="app.url"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-blue-500 hover:underline"
+                    >{{ app.name }}</a
+                  >
+                </h2>
+                <p class="text-gray-500">{{ app.description }}</p>
+
+                <p class="text-sm text-gray-500">
+                  <strong>Commission: </strong>{{ app.commission }}% ({{
+                    app.timeframe ? app.timeframe : 'recurring'
+                  }})
+                  <br />
+                  <a
+                    :href="app.programUrl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 mb-2 mt-3"
+                    >Join Affiliate Program</a
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="p-4">
+      <h2 class="text-2xl font-bold mb-2">What is MicroSaaS?</h2>
+      <p class="mb-4">
+        MicroSaaS is a unique subset of the Software as a Service (SaaS) model.
+        MicroSaaS businesses are typically small, owner-operated companies that
+        are highly specialized. They are often run by solo founders or small
+        teams and serve a niche market.
+      </p>
+      <p class="mb-6">
+        MicroSaaS businesses thrive by focusing on a narrow problem and
+        providing a simple, effective solution. They typically require less
+        capital to start and operate than larger SaaS businesses and can become
+        profitable more quickly.
+      </p>
+
+      <h2 class="text-2xl font-bold mb-2">Benefits of Affiliate Programs</h2>
+      <p class="mb-4">
+        Affiliate programs offer an excellent opportunity to generate passive
+        income. By promoting products or services, affiliates can earn a
+        commission on any sales made through their referral link.
+      </p>
+      <p class="mb-6">
+        These programs are particularly popular in the MicroSaaS space, where
+        businesses are always looking for cost-effective ways to reach their
+        target audience. Affiliates can help drive traffic and sales, making it
+        a win-win for both parties.
+      </p>
+
+      <h2 class="text-2xl font-bold mb-2">
+        Choosing the Right MicroSaaS Affiliate Program
+      </h2>
+      <p class="mb-4">
+        When choosing an affiliate program, it's important to consider factors
+        like the commission rate, payment terms, and the relevance of the
+        product or service to your audience. It's also a good idea to use the
+        product yourself so you can promote it authentically.
+      </p>
+      <p class="mb-6">
+        Remember, the key to successful affiliate marketing is trust. Your
+        audience needs to trust that you're recommending products because you
+        genuinely believe they're valuable, not just because you're getting
+        paid.
+      </p>
+    </section>
+
+    <!-- Footer -->
+    <footer class="p-4 bg-gray-200 text-center">
+      Made with ❤️ by
+      <a
+        href="https://easyretro.io"
+        target="_blank"
+        class="text-blue-500 hover:underline"
+        >EasyRetro</a
+      >
+    </footer>
+  </div>
+</template>
+
+<script>
+import { programs } from './_programs'
+
+export default {
+  data() {
+    return {
+      apps: programs,
+    }
+  },
+}
+</script>
